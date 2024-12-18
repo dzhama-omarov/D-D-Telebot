9cd012f3bc6884ad94ec9fa66e042bff79f30204
Dzhamaluddin Omarov <dzhamaluddin.omarov@gmail.com>
2024-12-18 20:47:26 +0300

    Update callbacks.py
    Translated to english

--- bot/callbacks.py
+++ bot/callbacks.py
@@ -14,15 +14,15 @@ def register_callbacks(bot: TeleBot):
                                     call.message.chat.id
                                     ) == BotStates.START.name)
     def create_character(call):
-        '''Данная функция вызываетсся, когда пользователь выбирает создание
-        персонажа.
-        Она записывает данное действие в логи, меняет состояние и
-        отправляет ответное сообщение пользователю сообщение
-        с просьбой выбрать рассу.
-        Также создается соответсвующая клавиатура'''
+"'This function is called when the user chooses to create
+        a character.
+        It records this action in the logs, changes the state and
+        sends a reply message to the user.
+        with a request to choose a race.
+        A corresponding keyboard is also created"'
         cur_user_id = call.from_user.id
-        cur_chat_id = call.message.chat.id
-        logger.info('{time}: Пользователь {user}, чат {chat_id} отправил callback {command}'
+cur_chat_id = call.message.chat.id
+logger.info ('{time}: User {user}, chat {chat_id} sent a callback {command}'
                     .format(time=datetime.fromtimestamp(call.message.date),
                             user=cur_user_id,
                             chat_id=cur_chat_id,
@@ -40,78 +40,78 @@ def register_callbacks(bot: TeleBot):
             character.level = 1
             character.equipment = ''
             character.save()
-            logger.info('{time}: Персонаж пользователя {user_id}, чат {chat_id} добавлен в бд'
-                        .format(time=datetime.fromtimestamp(call.message.date),
+            logger.info ('{time}: User character {user_id}, chat {chat_id} added to the database'
+.format(time=datetime.fromtimestamp(call.message.date),
                                 user_id=cur_user_id,
                                 chat_id=cur_chat_id
                                 ))
-        else:
-            logger.info('{time}: Персонаж пользователя {user_id}, чат {chat_id} найден в бд'
-                        .format(time=datetime.fromtimestamp(call.message.date),
+else:
+logger.info ('{time}: User character {user_id}, chat {chat_id} found in db'
+.format(time=datetime.fromtimestamp(call.message.date),
                                 user_id=cur_user_id,
                                 chat_id=cur_chat_id
                                 ))
-        bot.set_state(cur_user_id, BotStates.WAITING_FOR_RACE, cur_chat_id)
-        logger.info('{time}: Установлено состояние: {state}'
+bot.set_state(cur_user_id, BotStates.WAITING_FOR_RACE, cur_chat_id)
+logger.info ('{time}: Set state: {state}'
                     .format(time=datetime.fromtimestamp(call.message.date),
                             state=bot.get_state(cur_user_id, cur_chat_id)
-                            ))
-        bot.send_message(call.message.chat.id, 'Выбери расу',
+))
+bot.send_message(call.message.chat.id , 'Choose a race',
                          reply_markup=race_keyboard())
 
     @bot.callback_query_handler(func=lambda call:
                                 call.data.startswith('chosen_race:'))
-    def choose_class(call):
-        '''Данная функция вызываетсся, когда пользователь выбирает расу.
-        Она записывает данное действие в логи, меняет состояние и
-        отправляет ответное сообщение пользователю сообщение
-        с просьбой выбрать класс.
-        Также создается соответсвующая клавиатура'''
+def choose_class(call):
+"'This function is called when the user selects a race.
+        It records this action in the logs, changes the state and
+        sends a reply message to the user.
+        with a request to choose a class.
+        A corresponding keyboard is also created"'
         cur_user_id = call.from_user.id
-        cur_chat_id = call.message.chat.id
-        logger.info('{time}: Пользователь {user}, чат {chat_id} отправил callback {command}'
+cur_chat_id = call.message.chat.id
+logger.info ('{time}: User {user}, chat {chat_id} sent a callback {command}'
                     .format(time=datetime.fromtimestamp(call.message.date),
                             user=call.message.from_user.id,
                             chat_id=cur_chat_id,
                             command=call.data))
-        character = Character.get(Character.owner == cur_user_id)
-        character.race = call.data.split(':')[1]
-        character.save()
-        logger.info('{time}: Добавлена раса {race} в бд'
-                    .format(time=datetime.fromtimestamp(call.message.date),
+        character = Character.get(Character.owner ==cur_user_id)
+character.race = call.data.split(':')[1]
+character.save()
+logger.info ('{time}: Race {race} added to the database'
+.format(time=datetime.fromtimestamp(call.message.date),
                             race=call.data.split(':')[1]))
         bot.set_state(cur_user_id, BotStates.WAITING_FOR_CLASS, cur_chat_id)
-        logger.info('{time}: Установлено состояние: {state}'
+logger.info ('{time}: Set state: {state}'
                     .format(time=datetime.fromtimestamp(call.message.date),
                             state=bot.get_state(cur_user_id, cur_chat_id)
-                            ))
-        bot.send_message(call.message.chat.id, 'Выбери класс',
+))
+bot.send_message(call.message.chat.id , 'Choose a class',
                          reply_markup=class_keyboard())
 
     @bot.callback_query_handler(func=lambda call:
                                 call.data.startswith('chosen_class:'))
     def set_name(call):
-        '''Данная функция вызываетсся, когда пользователь выбирает класс.
-        Она записывает данное действие в логи, меняет состояние и
-        отправляет ответное сообщение пользователю сообщение
-        с просьбой назвать героя.
-        Также создается соответсвующая клавиатура'''
+        "'This function is called when the user selects a class.
+        It records this action in the logs, changes the state and
+        sends a reply message to the user.
+        with a request to name the hero.
+        A corresponding keyboard is also created"'
         cur_user_id = call.from_user.id
-        cur_chat_id = call.message.chat.id
-        logger.info('{time}: Пользователь {user}, чат {chat_id} отправил callback {command}'
+cur_chat_id = call.message.chat.id
+logger.info ('{time}: User {user}, chat {chat_id} sent a callback {command}'
                     .format(time=datetime.fromtimestamp(call.message.date),
                             user=call.message.from_user.id,
                             chat_id=cur_chat_id,
                             command=call.data))
-        character = Character.get(Character.owner == cur_user_id)
-        character.char_class = call.data.split(':')[1]
-        character.save()
-        logger.info('{time}: Добавлен класс {cclass} в бд'
-                    .format(time=datetime.fromtimestamp(call.message.date),
+        character = Character.get(Character.owner ==cur_user_id)
+character.char_class = call.data.split(':')[1]
+character.save()
+logger.info ('{time}: Added the {cclass} class to the database'
+.format(time=datetime.fromtimestamp(call.message.date),
                             cclass=call.data.split(':')[1]))
-        bot.set_state(user_id=cur_user_id, state=BotStates.WAITING_FOR_NAME, chat_id=cur_chat_id)
-        logger.info('{time}: Установлено состояние: {state}'
+bot.set_state(user_id=cur_user_id, state=BotStates.WAITING_FOR_NAME, chat_id=cur_chat_id)
+logger.info ('{time}: Set state: {state}'
                     .format(time=datetime.fromtimestamp(call.message.date),
                             state=bot.get_state(cur_user_id, cur_chat_id)
-                            ))
-        bot.send_message(call.message.chat.id, 'Как зовут героя?')
+))
+bot.send_message(call.message.chat.id , 'What's the hero's name?')
