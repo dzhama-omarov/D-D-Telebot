d5a23fbe8ac9b5c49c8e83d3fdb0f4833d9cd4bc
Dzhamaluddin Omarov <dzhamaluddin.omarov@gmail.com>
2024-12-18 20:46:25 +0300

    Update handlers.py
    Translated to english

--- bot/handlers.py
+++ bot/handlers.py
@@ -8,20 +8,20 @@ from database.models import Character, User
 
 def register_handlers(bot: TeleBot):
     @bot.message_handler(commands=['start'])
-    def main_menu(message):
-        '''Данная функция вызываетсся, когда пользователь отправляет /start.
-        Она записывает данное действие в логи и отправляет ответное сообщение
-        пользователю сообщение с подсказками о дальнейших дейстивиях.
-        Также создается клавиатура ответа'''
+def main_menu(message):
+"'This function is called when the user sends /start.
+        She records this action in the logs and sends a reply message
+        a message with hints about further actions is sent to the user.
+        A response keyboard is also created"'
         cur_user_id = message.from_user.id
-        cur_chat_id = message.chat.id
-        logger.info('{time}: Пользователь {user_id}, чат {chat_id} отправил команду {command}'
+cur_chat_id = message.chat.id
+logger.info ('{time}: User {user_id}, chat {chat_id} sent the command {command}'
                     .format(time=datetime.fromtimestamp(message.date),
                             user_id=cur_user_id,
                             chat_id=cur_chat_id,
                             command=message.text))
-        bot.set_state(user_id=cur_user_id, state=BotStates.START, chat_id=cur_chat_id)
-        logger.info('{time}: Установлено состояние: {state}'
+bot.set_state(user_id=cur_user_id, state=BotStates.START, chat_id=cur_chat_id)
+logger.info ('{time}: Set state: {state}'
                     .format(time=datetime.fromtimestamp(message.date),
                             state=bot.get_state(cur_user_id, cur_chat_id)
                             ))
@@ -30,31 +30,31 @@ def register_handlers(bot: TeleBot):
             defaults={'chat_id': cur_chat_id}
             )
         if created:
-            logger.info('{time}: Пользователь {user_id}, чат {chat_id} добавлен в бд'
-                        .format(time=datetime.fromtimestamp(message.date),
+            logger.info ('{time}: User {user_id}, chat {chat_id} added to the database'
+.format(time=datetime.fromtimestamp(message.date),
                                 user_id=cur_user_id,
                                 chat_id=cur_chat_id
                                 ))
-        else:
-            logger.info('{time}: Пользователь {user_id}, чат {chat_id} найден в бд'
-                        .format(time=datetime.fromtimestamp(message.date),
+else:
+logger.info ('{time}: User {user_id}, chat {chat_id} found in the database'
+.format(time=datetime.fromtimestamp(message.date),
                                 user_id=cur_user_id,
                                 chat_id=cur_chat_id
                                 ))
-        bot.send_message(message.chat.id, 'Приветствую тебя!\n'
-                         'Я помогу тебе создать героя. '
-                         'Если готов нажимай "Создать героя"'
-                         'Если же хочешь узнать, что я умею, нажми "Помощь"',
+bot.send_message(message.chat.id , 'Greetings to you!\n'
+'I'll help you create a hero. '
+'If you're ready, click "Create a hero"'
+                         'If you want to know what I can do, click "Help"',
                          reply_markup=main_menu_keyboard())
 
     @bot.message_handler(state=BotStates.WAITING_FOR_NAME)
-    def set_name(message):
-        '''Данная функция вызываетсся, когда пользователь отправляет любой
-        текст для ввода имени героя.
-        Она записывает данное действие в логи и отправляет ответное сообщение
-        пользователю сообщение с ингформацией о созданом герое'''
+def set_name(message):
+"'This function is called when the user sends any
+        text for entering the name of the hero.
+        She records this action in the logs and sends a reply message
+        a message to the user with information about the created hero"'
         cur_user_id = message.from_user.id
-        logger.info('{time}: Пользователь {user_id}, чат {chat_id} отправил имя героя {hero}'
+        logger.info ('{time}: User {user_id}, chat {chat_id} sent the name of the hero {hero}'
                     .format(time=datetime.fromtimestamp(message.date),
                             user_id=message.from_user.id,
                             chat_id=message.chat.id,
@@ -62,12 +62,12 @@ def register_handlers(bot: TeleBot):
         character = Character.get(Character.owner == cur_user_id)
         character.name = message.text
         character.save()
-        bot.send_message(message.chat.id, 'Герой создан!\n\n'
-                         'Имя: {name}\n'
-                         'Раса: {race}\n'
-                         'Класс: {cclass}\n'
-                         'Кол-во монет: {balance}\n'
-                         'Уровень: {level}'.format(name=character.name,
+bot.send_message(message.chat.id , 'A hero has been created!\n\n'
+                         'Name: {name}\n'
+                         'Race: {race}\n'
+                         'Class: {cclass}\n'
+                         'Number of coins: {balance}\n'
+                         'Level: {level}'.format(name=character.name,
                                                    race=character.race,
                                                    cclass=character.char_class,
                                                    balance=character.balance,
@@ -75,7 +75,7 @@ def register_handlers(bot: TeleBot):
 
     @bot.message_handler(func=lambda message: True)
     def debug_handler(message):
-        logger.info("Получено сообщение: {msg}".format(msg=message.text))
-        logger.info("Текущее состояние: {state}".format(
+        logger.info("Message received: {msg}".format(msg=message.text))
+        logger.info("Current state: {state}".format(
             state=bot.get_state(message.from_user.id, message.chat.id)
         ))
